8136f8be45019c67cd5ee8ade6504e9a47078608
puzevich <s.rudaya@sam-solutions.com>
2025-11-30 01:10:24 +0300

    fix: doc url for documentation

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     .setVersion('1.0.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('doc', app, document);
   writeFileSync('./doc/api.yaml', dump(document));
   app.useGlobalPipes(
     new ValidationPipe({
